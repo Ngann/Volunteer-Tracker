bd15ea72604f1d28c6ade2150af599840e7e65b2
Ngan Nguyen <ngancamnguyen@gmail.com>
2018-12-07 11:21:08 -0800

    show title spec passed for project class

--- lib/project.rb
+++ lib/project.rb
@@ -1,1 +1,12 @@
 require ('spec_helper')
+
+class Project
+  attr_accessor(:title)
+  attr_reader(:id)
+
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
+  end
+
+end
--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,1 +1,12 @@
 require ('spec_helper')
+
+class  Volunteer
+  attr_accessor(:name, :project_id)
+  attr_reader(:id)
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
+  end
+end
