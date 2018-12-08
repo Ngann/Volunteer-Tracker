53a5f9b3f4880de58b9a2b7df424ae761939d439
Ngan Nguyen <ngancamnguyen@gmail.com>
2018-12-07 16:36:42 -0800

    show == method passed spec for volunteer class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -9,4 +9,8 @@ class  Volunteer
     @project_id = attributes.fetch(:project_id)
     @id = attributes.fetch(:id)
   end
+
+  def ==(another_volunteer)
+    self.name().==(another_volunteer.name()).&(self.id().==(another_volunteer.id()))
+  end
 end
